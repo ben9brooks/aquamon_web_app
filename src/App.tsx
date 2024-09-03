6cafc2d9d2d49ce67010a80d5240ac86275416e9
Ben Brooks <ben9brooks@gmail.com>
2024-09-03 13:24:39 -0500

    adding routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,96 +1,100 @@
 import { GlobalStyle } from './styles/GlobalStyle'
-import { DebugToggleStyle } from './styles/DebugToggle';
-import { useState, useEffect } from 'react';
+import { DebugToggleStyle } from './styles/DebugToggle'
+import { useState, useEffect } from 'react'
 
 // let debug_mode = false;
 
 interface Sensors {
-  temp: number;
-  ph: number;
-  tds: number;
+  temp: number
+  ph: number
+  tds: number
 }
 
 const dummy: Sensors = {
   temp: -1,
   ph: -1,
-  tds: -1
-};
+  tds: -1,
+}
 
 async function fetchSensorData(isDebug: boolean): Promise<Sensors> {
-  const addr = 'http://192.168.0.175/sensors';
-  const mockAddr = 'https://66cca760a4dd3c8a71b860e1.mockapi.io/sensors';
-  console.log("Debug Mode is", isDebug);
-  if(isDebug) {
-    console.log("fetching", mockAddr);
-    const response = await fetch(mockAddr);
-    console.log("response", response);
-    if( response.status >= 200 && response.status < 300 ) {
-      const data = await response.json(); 
-      console.log("returning data and setting sensor var", data);
-      return data[0];
+  const addr = 'http://192.168.0.175/sensors'
+  const mockAddr = 'https://66cca760a4dd3c8a71b860e1.mockapi.io/sensors'
+  console.log('Debug Mode is', isDebug)
+  if (isDebug) {
+    console.log('fetching', mockAddr)
+    const response = await fetch(mockAddr)
+    console.log('response', response)
+    if (response.status >= 200 && response.status < 300) {
+      const data = await response.json()
+      console.log('returning data and setting sensor var', data)
+      return data[0]
     }
-    
   } else {
     try {
-      console.log("fetching", addr);
-      const response = await fetch(addr);
-      console.log("response", response);
-      if( response.status >= 200 && response.status < 300 ) {
-        const data = await response.json(); 
-        console.log("returning data and setting sensor var", data);
-        return data;
+      console.log('fetching', addr)
+      const response = await fetch(addr)
+      console.log('response', response)
+      if (response.status >= 200 && response.status < 300) {
+        const data = await response.json()
+        console.log('returning data and setting sensor var', data)
+        return data
       } else {
-        return dummy;
+        return dummy
       }
     } catch (e) {
       //something here to display in UI that an error has occurred
-      return dummy;
+      return dummy
     }
   }
-  return dummy;
+  return dummy
 }
 
-function getSensorClasses(val: number, warn_min: number, warn_max: number, hard_min: number, hard_max: number): string {
-  let classes = "circle ";
-  if ( val == -1 ) {
-    classes += "black";
-  } else if (val <= hard_min || val >= hard_max ) {
-    classes += "red";
-  } else if (val <= warn_min || val >= warn_max ) {
-    classes += "yellow"
+function getSensorClasses(
+  val: number,
+  warn_min: number,
+  warn_max: number,
+  hard_min: number,
+  hard_max: number
+): string {
+  let classes = 'circle '
+  if (val == -1) {
+    classes += 'black'
+  } else if (val <= hard_min || val >= hard_max) {
+    classes += 'red'
+  } else if (val <= warn_min || val >= warn_max) {
+    classes += 'yellow'
   } else {
-    classes += "green"
+    classes += 'green'
   }
   return classes
 }
 
-function Sensors({isDebug}: {isDebug: boolean}) {
-  const [sensor, setSensorData] = useState<Sensors>(dummy);
+function Sensors({ isDebug }: { isDebug: boolean }) {
+  const [sensor, setSensorData] = useState<Sensors>(dummy)
   // const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const data = await fetchSensorData(isDebug);
-        setSensorData(data);
+        const data = await fetchSensorData(isDebug)
+        setSensorData(data)
         // debugger;
       } catch (error) {
-        console.log("got error while fetching", error);
+        console.log('got error while fetching', error)
         // setError(error.message);
       }
     }
-    fetchData();
+    fetchData()
 
     // set to call itself at 5 second intervals
-    const intervalId = setInterval(fetchData, 5000);  
+    const intervalId = setInterval(fetchData, 5000)
 
-    return () => clearInterval(intervalId); // removes interval if sensor isn't rendered
-  }, [isDebug]);
-
+    return () => clearInterval(intervalId) // removes interval if sensor isn't rendered
+  }, [isDebug])
 
   if (sensor) {
     return (
-      <div className='sensor-row'>
+      <div className="sensor-row">
         <div className={getSensorClasses(sensor.temp, 50, 80, 40, 90)}>
           <p>{sensor.temp}°F</p>
         </div>
@@ -103,7 +107,7 @@ function Sensors({isDebug}: {isDebug: boolean}) {
           <p>{sensor.tds} ppm</p>
         </div>
       </div>
-    );
+    )
   } else {
     return (
       <div>
@@ -113,20 +117,16 @@ function Sensors({isDebug}: {isDebug: boolean}) {
   }
 }
 
-
-function ToggleDebug({ isDebug, toggleDebug }: { isDebug: boolean, toggleDebug: () => void }) {
-  // const [isDebug, setDebug] = useState(false);
-
-  // const handleClick = () => {
-  //   setDebug(!isDebug);
-  // };
-
+function ToggleDebug({
+  isDebug,
+  toggleDebug,
+}: {
+  isDebug: boolean
+  toggleDebug: () => void
+}) {
   return (
-    // <button onClick={toggleDebug} className={`toggle-button ${isDebug ? 'on' : 'off'}`}>
-    //   {isDebug ? "Debug Mode On" : "Debug Mode Off"}
-    // </button>
     <>
-      <p className='toggle-text'>{isDebug ? "Debug On" : "Debug Off"}</p>
+      <p className="toggle-text">{isDebug ? 'Debug On' : 'Debug Off'}</p>
       <label className="switch toggle-button">
         <input type="checkbox" onClick={toggleDebug} />
         <span className="slider"></span>
@@ -135,14 +135,12 @@ function ToggleDebug({ isDebug, toggleDebug }: { isDebug: boolean, toggleDebug: 
   )
 }
 
-
-
 export function App() {
-  const [isDebug, setDebug] = useState(false);
+  const [isDebug, setDebug] = useState(false)
   const toggleDebug = () => {
-    console.log("Changing debug from", isDebug, "to", !isDebug);
-    setDebug(!isDebug);
-    console.log("isDebug is now", isDebug);
+    console.log('Changing debug from', isDebug, 'to', !isDebug)
+    setDebug(!isDebug)
+    console.log('isDebug is now', isDebug)
   }
 
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,32 @@
-import ReactDOM from "react-dom";
-import { App } from "./App";
+import ReactDOM from 'react-dom/client'
+import { App } from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import * as React from 'react'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const router = createBrowserRouter([
+  {
+    path: '/main_window',
+    element: <App />,
+  },
+  {
+    path: '/temp',
+    element: <div>temp page </div>,
+  },
+  {
+    path: '/ph',
+    element: <div>ph page </div>,
+  },
+  {
+    path: '/tds',
+    element: <div>tds page </div>,
+  },
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
+
+// ReactDOM.render(<App />, document.getElementById('root'))
