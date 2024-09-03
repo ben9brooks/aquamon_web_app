310fa12a740000abe1727217844ff357c5454acf
Garrett <godh2p@umsystem.edu>
2024-09-03 15:45:24 -0500

    running on mac

--- server/main.js
+++ server/main.js
@@ -5,7 +5,7 @@ const path = require("path");
 // const moment = require("moment"); // Or use the Date object directly!!!!!!
 
 const app = express();
-const port = 5000;
+const port = 5001;
 
 // Open the existing database
 const db = Database("database.db");
